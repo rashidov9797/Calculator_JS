b0a068ca9a1456c9e4b7b24ea1ac48bbd18c404f
rashidov9797 <78200967+rashidov9797@users.noreply.github.com>
2021-09-01 16:28:26 +0900

    First_commit

---
+++ Calculator/main.js
@@ -0,0 +1,15 @@
+
+
+
+function insrt(num){
+  $(".display").val($(".display").val() + num);
+}
+
+function sql(){
+  $('.display').val(eval($('.display').val()));
+}
+
+function clr(){
+  $('.display').val('');
+}
+
